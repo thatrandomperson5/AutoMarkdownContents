fe9da34b49918c9574bbd1aaed08921d349f988f
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-12-11 12:07:03 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='AutoMarkdownContents',
+      version='1.0',
+      description='Python auto MD table of contents generator',
+      author='thatrandomperson5',
+      url='https://github.com/thatrandomperson5/AutoMarkdownContents/',
+      packages=['AutoMarkdownContents'],
+      install_requires=['mistletoe'],
+     )
